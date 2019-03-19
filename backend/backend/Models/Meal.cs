5d4d7f4e5d1e5652eceec2d4fd5313bd858c8de6
mesuchan <seminanele@gmail.com>
2019-03-19 13:29:59 +0300

    Models were added.

---
+++ backend/backend/Models/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public uint Weight { get; set; }
+        public uint Calorie { get; set; }
+        public decimal Price { get; set; }
+
+        public int MealCategoryId { get; set; }
+        public MealCategory MealCategory { get; set; }
+    }
+}
---
+++ backend/backend/Models/MealCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class MealCategory
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CanteenId { get; set; }
+        public Canteen Canteen { get; set; }
+    }
+}
