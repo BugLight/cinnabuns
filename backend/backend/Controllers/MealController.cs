4619bbb2400828ecc1141ffb484064710b29dba3
mesuchan <seminanele@gmail.com>
2019-03-19 18:53:08 +0300

    Functions GetCanteenMeals was added.

--- backend/backend/Controllers/MealController.cs
+++ backend/backend/Controllers/MealController.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using backend.Models;
+
 namespace backend.Controllers
 {
     [Route("api")]
     public class MealController : Controller
     {
+        public readonly AppContext context;
+
         // GET: api/canteens/5/meals
         [HttpGet("canteens/{canteenId}/meals")]
-        public IEnumerable<string> GetCanteenMeals(int canteenId)
+        public IEnumerable<Meal> GetCanteenMeals(int? canteenId, int? mealCategoryId, int? priceMin, int? priceMax, int? calorieMin, int? calorieMax)
         {
-            return new string[] { canteenId.ToString() };
+            return (from m in context.Meals
+                    join c in context.MealCategories on m.MealCategoryId equals c.Id
+                    where (c.CanteenId == canteenId &&
+                         (mealCategoryId == null || m.MealCategoryId == mealCategoryId) &&
+                         (priceMin == null || priceMin <= m.Price) &&
+                         (priceMax == null || priceMax >= m.Calorie) &&
+                         (calorieMin == null || calorieMin <= m.Calorie) &&
+                         (calorieMax == null || calorieMax >= m.Calorie))
+                    select m);
         }
 
         // GET api/meals/3
