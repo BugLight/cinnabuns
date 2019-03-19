da3cf14fd159878f88c66cdfdfe0b1dd4943fc1b
buglight <buglight@kistriver.com>
2019-03-19 11:28:51 +0300

    Database context was added

---
+++ backend/backend/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
