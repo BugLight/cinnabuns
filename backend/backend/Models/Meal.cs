d3357db7eb9fa820761a69a95141e2ca1ce8900a
buglight <buglight@kistriver.com>
2019-03-20 22:21:49 +0300

    Fixed EF bug: now using int instead of uint

--- backend/backend/Models/Meal.cs
+++ backend/backend/Models/Meal.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
-        public uint Weight { get; set; }
-        public uint Calorie { get; set; }
+        public int Weight { get; set; }
+        public int Calorie { get; set; }
         public decimal Price { get; set; }
 
         public int MealCategoryId { get; set; }
